4fed9bcb60a0182850b30ce519faa93726a1cba7
Rene Hexel <r.hexel@griffith.edu.au>
2016-05-15 16:14:51 +1000

    Add Xcode project

---
+++ SwiftLibXML/SwiftLibXML.h
@@ -0,0 +1,19 @@
+//
+//  SwiftLibXML.h
+//  SwiftLibXML
+//
+//  Created by Rene Hexel on 15/05/2016.
+//  Copyright © 2016 Rene Hexel. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftLibXML.
+FOUNDATION_EXPORT double SwiftLibXMLVersionNumber;
+
+//! Project version string for SwiftLibXML.
+FOUNDATION_EXPORT const unsigned char SwiftLibXMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLibXML/PublicHeader.h>
+
+
