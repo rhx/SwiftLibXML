d11ee96694eb2a0e2b0f68bbb3d68451f5db91c5
Rene Hexel <r.hexel@griffith.edu.au>
2021-12-26 06:03:56 +1000

    Make ParserOptions conform to OptionSet and simplify initialiser

--- Sources/SwiftLibXML/XMLDocument.swift
+++ Sources/SwiftLibXML/XMLDocument.swift
@@ -3,13 +3,12 @@
 //  SwiftLibXML
 //
 //  Created by Rene Hexel on 24/03/2016.
-//  Copyright © 2016, 2018, 2020 Rene Hexel. All rights reserved.
+//  Copyright © 2016, 2018, 2020, 2021 Rene Hexel. All rights reserved.
 //
+import Foundation
 #if os(Linux)
-    import Glibc
     import CLibXML2
 #else
-    import Darwin
     import libxml2
 #endif
 
@@ -17,27 +16,33 @@
 /// A wrapper around libxml2 xmlDoc
 ///
 public class XMLDocument {
-    let xml: xmlDocPtr
-    let ctx: xmlParserCtxtPtr? = nil
+    public typealias ParserOptions = xmlParserOption
+    /// The underlying XML document
+    @usableFromInline let xml: xmlDocPtr
+//    let ctx: xmlParserCtxtPtr? = nil
 
     /// private constructor from a libxml document
-    init(xmlDocument: xmlDocPtr) {
+    /// - Parameter xmlDocument: The XML document to parse
+    @inlinable public init(xmlDocument: xmlDocPtr) {
         xml = xmlDocument
         xmlInitParser()
     }
 
-    /// failable initialiser from memory with a given parser function
-    public convenience init?(buffer: UnsafeBufferPointer<CChar>, options: Int32 = Int32(XML_PARSE_NOWARNING.rawValue | XML_PARSE_NOERROR.rawValue | XML_PARSE_RECOVER.rawValue), parser: ((UnsafePointer<CChar>?, Int32, UnsafePointer<CChar>?, UnsafePointer<CChar>?, Int32) -> xmlDocPtr?)? = nil) {
-        guard let base = buffer.baseAddress else { return nil }
-        let maybeXML: xmlDocPtr?
-        if let parse = parser { maybeXML = parse(base, Int32(buffer.count), "", nil, options) }
-        else { maybeXML = xmlReadMemory(base, Int32(buffer.count), "", nil, options) }
-        guard let xml = maybeXML else { return nil }
+    /// Failable initialiser from memory with a given parser function
+    /// - Parameters:
+    ///   - buffer: The buffer containing the XML to parse
+    ///   - options: The parser options to use
+    ///   - parser: The parser to use; defaults to`xmlReadMemory`
+    @inlinable public convenience init?(buffer: UnsafeBufferPointer<CChar>, options: ParserOptions = [.noWarning, .noError, .recover, .noNet], parser parse: ((UnsafePointer<CChar>?, Int32, UnsafePointer<CChar>?, UnsafePointer<CChar>?, Int32) -> xmlDocPtr?) = xmlReadMemory) {
+        guard let base = buffer.baseAddress,
+              let xml = parse(base, Int32(buffer.count), "", nil, Int32(bitPattern: options.rawValue)) else { return nil }
         self.init(xmlDocument: xml)
     }
 
-    /// initialise from a file
-    public convenience init?(fromFile fileName: UnsafePointer<CChar>, options: Int32 = Int32(XML_PARSE_NOWARNING.rawValue | XML_PARSE_NOERROR.rawValue | XML_PARSE_RECOVER.rawValue)) {
+    /// Initialise from an XML file
+    /// - Parameters:
+    ///   - fileName: The XML file to read
+    @inlinable public convenience init?(fromFile fileName: UnsafePointer<CChar>) {
         guard let xml = xmlParseFile(fileName) else { return nil }
         self.init(xmlDocument: xml)
     }
@@ -45,19 +50,22 @@ public class XMLDocument {
     /// clean up
     deinit {
         xmlFreeDoc(xml)
+//        if let ctx = ctx { xmlFreeParserCtxt(ctx) }
     }
 
-    /// get the root element
+    /// Return the tree's the root element
     public var rootElement: XMLElement {
         return XMLElement(node: xmlDocGetRootElement(xml))
     }
 
-    /// get the XML tree for enumeration
+    /// Return the XML tree for enumeration
     public var tree: XMLTree {
         return XMLTree(xml: self)
     }
 
-    /// get an attribute value
+    /// Get an attribute value
+    /// - Parameter attribute: The attriibute to get the value for
+    /// - Returns: A String containing the attribute value, or `nil` if nonexistent
     public func valueFor(attribute: XMLAttribute) -> String? {
         let attr = attribute.attr
         guard let children = attr.pointee.children,
@@ -74,6 +82,57 @@ public class XMLDocument {
     }
 }
 
+extension XMLDocument.ParserOptions: OptionSet {
+    /// Recover on errors
+    public static let recover = XML_PARSE_RECOVER
+    /// Substitute entitites
+    public static let noEntity = XML_PARSE_NOENT
+    /// Load external subsets
+    public static let loadExternalSubsets = XML_PARSE_DTDLOAD
+    /// Default DTD attributes
+    public static let defaultDTDAttributes = XML_PARSE_DTDATTR
+    /// Validate with the DTD
+    public static let validateDTD = XML_PARSE_DTDVALID
+    /// Suppress error reports
+    public static let noError = XML_PARSE_NOERROR
+    /// Suppress warning reports
+    public static let noWarning = XML_PARSE_NOWARNING
+    /// Pedantic error reporting
+    public static let pedantic = XML_PARSE_PEDANTIC
+    /// Remove blank nodes
+    public static let noBlanks = XML_PARSE_NOBLANKS
+    /// Use the SAX1 interface internally
+    public static let sa1 = XML_PARSE_SAX1
+    /// Implement XInclude substitition
+    public static let xInclude = XML_PARSE_XINCLUDE
+    /// Forbid network access
+    public static let noNet = XML_PARSE_NONET
+    /// Do not reuse the context dictionary
+    public static let noDictionary = XML_PARSE_NODICT
+    /// Remove redundant namespaces declarations
+    public static let nsClean = XML_PARSE_NSCLEAN
+    /// Merge CDATA as text nodes
+    public static let noCDATA = XML_PARSE_NOCDATA
+    /// Do not generate XINCLUDE START/END nodes
+    public static let noXIncludeNode = XML_PARSE_NOXINCNODE
+    /// Compact small text nodes
+    ///
+    /// No modification of the tree is allowed afterwards
+    /// (will possibly crash if you try to modify the tree)
+    public static let compact = XML_PARSE_COMPACT
+    /// Parse using XML-1.0 before update 5
+    public static let old10 = XML_PARSE_OLD10
+    /// Do not fixup XINCLUDE xml:base uris
+    public static let noBaseFix = XML_PARSE_NOBASEFIX
+    /// Relax any hardcoded limit from the parser
+    public static let huge = XML_PARSE_HUGE
+    /// Parse using SAX2 interface before update 5
+    public static let oldSAX = XML_PARSE_OLDSAX
+    /// Ignore internal document encoding hint
+    public static let ignoreEncoding = XML_PARSE_IGNORE_ENC
+    /// Store big line numbers in text PSVI field
+    public static let bigLines = XML_PARSE_BIG_LINES
+}
 
 //
 // MARK: - Enumerating XML
